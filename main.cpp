f6d5b3d22581e53beb3b04c7cf1e100e52c14267
César Augusto Romero Molano <ceroma_175@hotmail.com>
2022-12-05 20:46:46 +0000

    cambio definiciones

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 
-#define tiempo_rebote           100ms
+#define tiempo_rebote           5ms
 #define BLINKING_RATE           1000ms
 
 //static BufferedSerial serial_port(USBTX, USBRX);
@@ -8,14 +8,14 @@ static BufferedSerial serial_port(PA_2, PA_3);
 char mensaje[40]={0};
 
 //Variables globales
-char EstadoPresente_boton,EstadoAnterior_Boton;
+bool EstadoPresente_boton,EstadoAnterior_Boton;
 int contador=0;
 
 //Defino pines del bus de salida y pin del boton entrada
 DigitalOut miled(PA_5);//Led Tarjeta
 BusOut mi_BusSalida(PA_10,PB_5,PB_4,PB_10,PA_8,PA_9,PC_7,PB_6); //D2,D4,D5,D6,D7,D8,D9,D10
-DigitalIn mi_boton0(PC_13); //PC_13=Boton1 PB_3=D3 otro
-//DigitalIn mi_boton0(PB_3);//,PullUp);     //Usar con PB_3 si uso PC_13 sobra
+//DigitalIn mi_boton0(PC_13); //PC_13=Boton1 PB_3=D3 otro
+DigitalIn mi_boton0(PB_3);//,PullUp);     //Usar con PB_3 si uso PC_13 sobra
 
 // Prototipo de la funcion a usar en el hilo
 void Leer_Boton(void);
